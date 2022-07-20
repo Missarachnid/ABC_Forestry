e614e68fdfdf58dbb6a09ef264779cd0f58d9bf6
M. Kepler <mmkepler@yahoo.com>
2022-07-20 15:17:49 -0600

    feat: styled footer with copyright and twitter link

--- pages/footer/index.js
+++ pages/footer/index.js
@@ -1,8 +1,13 @@
 import footerStyles from "../../styles/Footer.module.css";
+import twitter from "../../public/images/Twitter_Social_Icon_Circle_White.png";
+import Image from "next/image";
 
 const Footer = () => (
   <footer className={footerStyles.footer}>
-    <p>ABC Forestry</p>
+    <p>ⓒ 2022 ABC Forestry</p>
+    <a href="https://www.twitter.com">
+      <Image src={twitter} alt="white twitter bird logo" height="32px" width="32px"/>
+    </a>
   </footer>
 );
 export default Footer;
